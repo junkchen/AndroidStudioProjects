3cc793fc1da92862e83e3f737ab4745de5c588f4
Junk Chen <junkchen@aliyun.com>
2018-09-30 10:51:05 +0800

    Update ChartDemo.

--- HetDemo/chartdemo/src/main/java/com/junkchen/chartdemo/widget/LineChart.kt
+++ HetDemo/chartdemo/src/main/java/com/junkchen/chartdemo/widget/LineChart.kt
@@ -1,4 +1,45 @@
 package com.junkchen.chartdemo.widget
 
-class LineChart {
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * 折线图
+ * Create by Junk Chen on 2018/9/29.
+ */
+class LineChart(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : View(
+        context, attrs, defStyleAttr
+) {
+
+    private val linePaint = Paint()
+    private val axisPaint = Paint()
+    private val textPaint = Paint()
+
+    init {
+        linePaint.color = Color.BLUE
+
+        axisPaint.color = Color.DKGRAY
+        axisPaint.strokeWidth = 4F
+
+        textPaint.color = Color.DKGRAY
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        // 偶数整点时间轴线
+
+        // 奇数整点时间轴线
+
+        // 半小时时间轴线
+
+    }
 }
