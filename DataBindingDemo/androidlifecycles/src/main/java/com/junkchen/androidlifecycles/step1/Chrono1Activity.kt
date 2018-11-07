06f1d474d77f4b55cc14caeeadc720033c624da5
Junk Chen <junkchen@aliyun.com>
2018-11-07 11:17:06 +0800

    Add new module: AndroidLifecycles.

---
+++ DataBindingDemo/androidlifecycles/src/main/java/com/junkchen/androidlifecycles/step1/Chrono1Activity.kt
@@ -0,0 +1,14 @@
+package com.junkchen.androidlifecycles.step1
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Chrono1Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chrono)
+
+        
+    }
+}
