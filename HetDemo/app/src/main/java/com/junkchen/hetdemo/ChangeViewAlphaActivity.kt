fda0f2c799a41c98e750eafae535c2571650cc5e
Junk Chen <junkchen@aliyun.com>
2018-12-04 18:06:14 +0800

    Update HetDemo.

---
+++ HetDemo/app/src/main/java/com/junkchen/hetdemo/ChangeViewAlphaActivity.kt
@@ -0,0 +1,12 @@
+package com.junkchen.hetdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangeViewAlphaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_view_alpha)
+    }
+}
